60d63db131bd36f31434c520d599685895fb4a6d
YossiElgazari <yossielg98@gmail.com>
2024-08-07 14:46:37 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use('/api', wordRoutes);
 
 // Start the server
 const server = app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at https://hangman-kxt2.onrender.com/${port}`);
 });
 
 module.exports = { server, app };
